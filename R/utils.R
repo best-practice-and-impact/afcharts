31bc5fd3ae098982b9f50f710f3677e898fab264
Olivia Box Power <Olivia.BoxPower@dhsc.gov.uk>
2024-08-20 13:31:23 +0100

    Temp commit

--- R/utils.R
+++ R/utils.R
@@ -17,3 +17,25 @@ mm_to_inch <- function(x) {
   x / 25.4
 
 }
+
+
+
+#' Label pounds sterling
+#'
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' scales::demo_continuous(c(0.1, 1), labels = label_pounds())
+#' scales::demo_continuous(c(1, 1e3), labels = label_pounds())
+#' scales::demo_log10(c(1, 1e12), breaks = log_breaks(5, 1e3), labels = label_pounds())
+label_pounds <- function(...) {
+  label_currency(
+    prefix = "£",
+    big.mark = ",",
+    decimal.mark = ".",
+    scale_cut = c(0, k = 1e3, m = 1e6, bn = 1e9, tn = 1e12),
+    ...)
+}
